2188753712d57db451c2f2df49dde60a35546601
Superjomn <yanchunwei@outlook.com>
2022-11-19 12:39:04 +0800

    finish bili mode

--- bili.el
+++ bili.el
@@ -2,13 +2,25 @@
   :type 'string
   :group 'chun-bili)
 
+(defcustom chun-bili--anki-note-type "Basic (and reversed card)" nil
+  :type 'string
+  :group 'chun-bili)
+
+(defcustom chun-bili--anki-dock "English-translate" nil
+  :type 'string
+  :group 'chun-bili)
+
+(defcustom chun-bili--en "En" nil
+  :type 'string
+  :group 'chun-bili)
+
+(defcustom chun-bili--ch "Ch" nil
+  :type 'string
+  :group 'chun-bili)
+
 (defconst chun-bili-anki-note-type-property-key "ANKI_NOTE_TYPE")
 (defconst chun-bili-anki-dock-property-key "ANKI_DOCK")
 
-(defconst chun-bili--note-type "Basic (and reversed card)")
-(defconst chun-bili--dock "English-translate")
-(defconst chun-bili--en "En")
-(defconst chun-bili--ch "Ch")
 
 (defun chun-bili-get-content-at-this-point ()
   "Get En and Ch content from the headline at this point."
@@ -34,7 +46,7 @@ There should be two subsection in this section, the first is 'En', the second is
         (org-element-map content-element
             'headline
           (lambda (elem)
-            (let* ((elem-title (org-element-property :raw-value elem)) elem-content)
+            (let* ((elem-title (string-trim (org-element-property :raw-value elem))) elem-content)
               (setq elem-content (buffer-substring-no-properties (org-element-property :contents-begin elem)
                                                                  (org-element-property :contents-end elem)))
               (message "elem: %S" content-element)
@@ -112,9 +124,6 @@ Returns:
     (org-element-put-property child-element-1 :title "Back")
     (org-element-put-property child-element-1 :level (+ headline-level 1))
 
-
-
-
     (org-element-adopt-elements element child-element-0 child-element-1)
 
     ;; (chun-bili--org-element-put-anki-properties element "EnCh" "Basic")
@@ -123,14 +132,14 @@ Returns:
     ))
 
 
-(defun chun-bili--pair-data-to-org (lines0 lines1 note-type anki-dock headline-level)
+(defun chun-bili--pair-data-to-org (title lines0 lines1 note-type anki-dock headline-level)
   "Transpose two list to org-mode content."
   (assert (eq (length lines0) (length lines1)))
   (let* ((i 0)
          (n (length lines0))
          line0
          line1
-         (org-content "")
+         (org-content (format "* %s\n" title))
          )
     (while (< i n)
         (setq line0 (nth i lines0))
@@ -146,30 +155,50 @@ Returns:
   "Write content to file."
   (save-excursion
     (let*
-        ((buffer (get-buffer-create "*CHUN-BILI*")))
+        ((buffer (get-file-buffer chun-bili-publish-file)))
       (with-current-buffer buffer
-        (goto-char (point-max)
-                   (insert content))))))
+        (goto-char (point-max))
+        (insert content)
+        ))))
 
+(defun chun-bili--string-valid (str)
+  (if (stringp str) (not (string= (string-trim str) ""))))
+
+(defun chun-bili--get-anki-note-type (element)
+  "Get anki note-type property. Return it if set or get the global variable `chun-bili--anki-note-type' instead."
+  (let* ((anki-note (org-element-property :ANKI_NOTE_TYPE element)))
+    (if (chun-bili--string-valid anki-note) anki-note
+      chun-bili--anki-note-type)))
+
+(defun chun-bili--get-anki-dock-name (element)
+  "Get anki note-type property. Return it if set or get the global variable `chun-bili--anki-dock' instead."
+  (let* ((anki-note (org-element-property :ANKI_DOCK element)))
+    (if (chun-bili--string-valid anki-note) anki-note
+      chun-bili--anki-dock)))
+
+(defun chun-bili--split-valid-lines (str)
+  (-filter #'chun-bili--string-valid
+           (mapcar #'string-trim (split-string str "\n"))))
 
 (defun chun-bili--pair-data-this ()
   (interactive)
   (let* ((pair-dic (chun-bili--get-ch-en-content-at-this-point))
          (content-en (ht-get pair-dic chun-bili--en))
          (content-ch (ht-get pair-dic chun-bili--ch))
-         (lines-en (split-string content-en "\n"))
-         (lines-ch (split-string content-ch "\n"))
+         (lines-en (chun-bili--split-valid-lines content-en))
+         (lines-ch (chun-bili--split-valid-lines content-ch))
          (element (org-element-at-point))
-         (anki-properties (chun-bili--get-anki-properties element))
-         (anki-note-type (car anki-properties))
-         (anki-dock (cdr anki-properties))
+         (title (org-element-property :raw-value element))
          org-content
          )
 
-    (setq org-content (chun-bili--pair-data-to-org lines-en lines-ch anki-note-type
-                                      anki-dock 1))
+         (setq anki-note-type (chun-bili--get-anki-note-type element))
+         (message "anki-note type %s" anki-note-type)
+         (setq anki-dock (chun-bili--get-anki-dock-name element))
+         (message "anki-dock %s" anki-dock)
+
+    (setq org-content (chun-bili--pair-data-to-org title lines-en lines-ch anki-note-type
+                                      anki-dock 2))
     (message "%S" org-content)
     (chun-bili--write-to-file org-content chun-bili-publish-file)))
 
-(chun-bili--create-anki-org-record "line0" "line1" "node-type" "en" 3)
-(chun-bili--pair-data-to-org '("a" "b") '("c" "d") "note-type" "anki-dock" 2)
